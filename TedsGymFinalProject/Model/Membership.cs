4590be92ecaec38515f35d8f104aa8970e403614
Tommy Wagner <thomas.wagner@marquette.edu>
2023-12-05 20:18:14 -0600

    Created Membership and Balance Classes.

--- TedsGymFinalProject/Model/Membership.cs
+++ TedsGymFinalProject/Model/Membership.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace TedsGymFinalProject.Model;
 
-internal class Membership
+public class Membership
 {
+    public int MembershipLvl { get; set; }
+
+    public bool Renewal { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public double Cost { get; set; }
+
+    
+    public Membership (int membershipLvl, bool isActive, bool renewal, double cost)
+    {
+        this.MembershipLvl = membershipLvl;
+        this.IsActive = isActive;
+        this.Renewal = renewal;
+        this.Cost = cost;
+    }
+
 
 
 
