66eb2d4a4c6210471237327393b70b015031fca5
Ted Lawder <thomas.lawder@marquette.edu>
2023-12-11 15:41:38 -0600

    Stuff

--- TedsGymFinalProject/Program.cs
+++ TedsGymFinalProject/Program.cs
@@ -11,7 +11,8 @@ public class Program
     private static Members members;
     private static Member member;
     private static Member authenticatedMember;
-
+    private static List<Appointment> appointments;
+    private static List<MemberAppointment> customerAppointments;
     static void Main(string[] args)
     {
         Console.WriteLine("Loading...");
@@ -35,6 +36,7 @@ public class Program
         members = new Members();
         members.members.Add(a1);
 
+        appointments = new List<Appointment>();
 
         Membership Premium = new Membership(3, "Premium", 100.00);
         Membership Plus = new Membership(2, "Plus", 75.00);
@@ -111,9 +113,35 @@ public class Program
                     Password = password,
                     PhoneNumber = phoneNumber,
                 };
+            }
 
-                Console.WriteLine("Choose a Membership Level:");
-                Console.WriteLine($"Basic (1), Plus (2), Premium (3)");
+            static void MembershipPlan()
+            {
+                int selectedPlan = 0;
+
+                while (selectedPlan < 1 || selectedPlan > 3)
+                {
+                    Console.WriteLine("Select a membership plan:");
+                    Console.WriteLine("1. Basic - $50.00");
+                    Console.WriteLine("2. Plus - $75.00");
+                    Console.WriteLine("3. Premium - $100.00");
+                    Console.WriteLine("Please select a plan by entering a number (1-3)");
+
+                        if (int.TryParse(Console.ReadLine(), out selectedPlan))
+                        {
+                            if(selectedPlan < 1 || selectedPlan > 3)
+                            {
+                                Console.WriteLine("Invalid input! Please enter a number from 1-3.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+        }
+    }
+
+            membershipLvl selectedMembershipLvl = (membershipLvl)selectedPlan;
+            Console.Write($"You have selected the {selectedMembershipLvl} membership plan.");
             }
 
 
@@ -157,7 +185,17 @@ public class Program
             }
             static void ViewAppointments()
             {
-
+                 var appointmentList = memberAppointments.Where(o => o.customer.Username == authenticatedMember.Username);
+                if(appointmentList.Count() == 0)
+                {
+                    Console.WriteLine("No appointment found.");
+                }
+                else
+                {
+                    foreach(var appointment in appointmentList)
+                    {
+                        Console.WriteLine(appointment.appointment.date);
+                    }
             }
             
 
