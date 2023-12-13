df45e3226b27c442fe4c38e6c1ea65ec31506cd5
John Horan <harrison.horan@marquette.edu>
2023-12-12 22:02:44 -0600

    This is the final code for the project. I am Going to Commit the changes, and push them to origin. Then I will merge them into development, then master.

--- TedsGymFinalProject/Model/Balance.cs
+++ TedsGymFinalProject/Model/Balance.cs
@@ -6,30 +6,27 @@ using System.Threading.Tasks;
 
 
 public class Balance
-{ 
-	public Balance()
-	{
-	}	
-		
-	public double TotalBalance {  get; set; }
-		
-	public double LateFees {  get; set; }
+{
 
-	public double Rewards { get; set; }
-		
-	public double Vending {  get; set; }
+    public Balance()  // Empty constructor
+    {
+    }
 
+    public double TotalBalance { get; set; }  // Creating objects with getter and setter
 
-	public Balance (double totalBalance, double lateFees, double rewards, double vending)
-	{
-		this.TotalBalance = totalBalance;
-		this.LateFees = lateFees;
-		this.Rewards = rewards;
-		this.Vending = vending;
-	}
+    public double LateFees { get; set; }
 
-		
+    public double Rewards { get; set; }
+
+    public double Vending { get; set; }
 
 
-	
+    public Balance(double totalBalance, double lateFees, double rewards, double vending)  // Constructor for Balance
+    {
+        this.TotalBalance = totalBalance;  // Using “this” to reference the objects
+        this.LateFees = lateFees;
+        this.Rewards = rewards;
+        this.Vending = vending;
+    }
+
 }
--- TedsGymFinalProject/Model/Membership.cs
+++ TedsGymFinalProject/Model/Membership.cs
@@ -6,26 +6,29 @@ using System.Threading.Tasks;
 
 namespace TedsGymFinalProject.Model;
 
-public class Membership
+public class Membership//Creates membership class
 {
 
-    public int MembershipLvl { get; set; }
+    public List<Membership> membership { get; set; }//creates list to enter membership in
 
-    public string MembershipName { get; set; }
-    public double Cost { get; set; }
-
-    
-    public Membership (int membershipLvl, string membershipName, double cost)
+    public Membership()//membership constructor to create new membership
     {
-        this.MembershipLvl = membershipLvl;
-        this.MembershipName = membershipName;
-        this.Cost = cost;
+        membership = new List<Membership>();//links membership created to list
     }
+    public int MembershipLvl { get; set; }//creates membership level variable
+
+    public string MembershipName { get; set; }//creates membership name variable
+    public double Cost { get; set; }//creates cost variable
 
 
+    public Membership(int membershipLvl, string membershipName, double cost)//creates constructor that you can input membershiplvl, membershipnamem and cost into
+    {
+        this.MembershipLvl = membershipLvl;//connects input to variable of membership
+        this.MembershipName = membershipName; //connects input to variable of membership
 
+        this.Cost = cost; //connects input to variable of membership
 
-
+    }
 
 
 }
